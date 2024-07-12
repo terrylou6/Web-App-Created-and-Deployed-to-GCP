93d056e8f88dfec9af6b1e6c846119440c4d8324
terrylou6 <60866527+terrylou6@users.noreply.github.com>
2024-07-12 00:41:19 -0500

    Update 2. flask build and run-Terry Lou.py
    Update a few coding to successfully run the flask app.

--- 2. flask build and run-Terry Lou.py
+++ 2. flask build and run-Terry Lou.py
@@ -3,32 +3,29 @@ import json
 import numpy as np
 import pickle
 
-
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='C:/ProgramData/anaconda3/Lib/site-packages/flask/templates')
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     pred = ""
     if request.method == "POST":
-        warehouse = request.form["warehouse (Input Integer 0-6)"]
-        holiday_name = request.form["holiday_name (Input Integer 0-24)"]
-        holiday = request.form["holiday (Input 0 or 1)"]
-        shops_closed = request.form["shops_closed (Input 0 or 1)"]
-        winter_school_holidays = request.form["winter_school_holidays (Input 0 or 1)"]
-        school_holidays = request.form["school_holidays (Input 0 or 1)"]
+        warehouse = request.form["warehouse"]
+        holiday_name = request.form["holiday_name"]
+        holiday = request.form["holiday"]
+        shops_closed = request.form["shops_closed"]
+        winter_school_holidays = request.form["winter_school_holidays"]
+        school_holidays = request.form["school_holidays"]
         year = request.form["year"]
-        month = request.form["month (Input Integer 1-12)"]
-        day = request.form["day (Input Integer 1-31)"]
-        dayofweek = request.form["dayofweek (Input Integer 0-6)"]
-        is_weekend = request.form["is_weekend (Input 0 or 1)"]
+        month = request.form["month"]
+        day = request.form["day"]
+        dayofweek = request.form["dayofweek"]
+        is_weekend = request.form["is_weekend"]
         X = np.array([[float(warehouse), float(holiday_name), float(holiday), float(shops_closed), float(winter_school_holidays), float(school_holidays), float(year), float(month), float(day), float(dayofweek), float(is_weekend)]])
-        pred = model.predict(X)
-    return render_template("index.html", pred=pred)
-
+        pred = model.predict(X)[0]
+    return render_template("index_new.html", pred=pred)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='127.0.0.1', port=5000)
